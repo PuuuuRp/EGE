d457d3c6f9ec57f97b321b55a15d2c17fc3aed77
AUshakov <chelovechina09@gmail.com>
2024-12-24 17:02:54 +0200

    Итоговое решение

--- ЕГКР-21-12-24/Зад 5 19237.py
+++ ЕГКР-21-12-24/Зад 5 19237.py
@@ -1,4 +1,18 @@
-print('Верните IDLE')
+def toq(n, q):
+    res = ''
+    while n:
+        res += str(n%q)
+        n//=q
+    return res[::-1]
 
-
-print('ВЕРНИТЕ!!!')
+res = []
+for n in range(1, 1000):
+    r  = toq(n, 3)
+    if n%3 == 0:
+        r += r[-2:]
+    else:
+        r += toq(sum([int(i) for i in r]),3)
+    if int(r, 3) > 220:
+        res.append(int(r, 3))
+print(min(res))
+#222
