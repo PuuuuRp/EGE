05937f1030aabd696bfe9284321c1eb82d3d7285
AUshakov <chelovechina09@gmail.com>
2025-01-07 17:44:11 +0200

    Ответы в файлах

--- 07-01-25/T8 13298.py
+++ 07-01-25/T8 13298.py
@@ -5,6 +5,7 @@ res = []
 for pos, val in enumerate(product(al, repeat=5), 1):
     val = ''.join(val)
     if all(i in sl and val.count(i)==1 for i in val) and pos%5 != 0:
-        res.append(pos-pos//5)
-print(res[0])
+        print(pos-pos//5)
+        print(val)
+        break
 #4754
