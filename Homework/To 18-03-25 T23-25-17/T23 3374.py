e9e7fc152be625951b183f5669eb21919b4b1aad
AUshakov <chelovechina09@gmail.com>
2025-03-18 15:50:30 +0200

    Добивка домашки на вторник 18 марта

--- Homework/To 18-03-25 T23-25-17/T23 3374.py
+++ Homework/To 18-03-25 T23-25-17/T23 3374.py
@@ -1,6 +1,16 @@
+res = set()
+c = 0
 def f(a, n):
-    if a==n: return 1
-    if a<-50 or a>50: return 0
-    return f(a+2, n)+f(a-3, n)
-print(f(1, 30)) 
-#
+    global res, c
+    if a==n:
+        c += 1
+        return
+    if a<-50 or a>50: return
+    if a in res: return
+    res.add(a)
+    f(a+2, n)
+    f(a-3, n)
+    res -= {a}
+f(1, 30)
+print(c)
+#851
