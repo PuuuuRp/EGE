6d9f01efa3b26be006f1fccb07a04fcd974d7a6e
AUshakov <chelovechina09@gmail.com>
2025-04-04 16:26:45 +0200

    самостоялка

--- Independent works/04-04-25/T8 17799.py
+++ Independent works/04-04-25/T8 17799.py
@@ -1,2 +1,12 @@
 print(int('4567', 8)+1)
 #2424
+####################################
+
+from itertools import *
+al = ''.join(sorted('АРГУМЕНТ'))
+#al = '01234567'
+for pos, val in enumerate(product(al, repeat=4), 1):
+    val = ''.join(val)
+    if val in al:
+        print(pos)
+#2424
